6259dce0de20f9933164b5e3b072806f18e48264
CesarPagliarini <capagliarini@gmail.com>
2021-12-11 11:00:54 -0300

    agregado de variables de entorno y construccion del string de conexion a la DB

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ const express = require('express');         // Framework web que permite crear A
 const router = require('./routes');         //
 const mongoose = require('mongoose');       // Libreria para el manejo de la base de datos
 const cors = require('cors');               // Se utiliza para matchear origenes cruzados
-require('dotenv/config');                   // Se utiliza para manejar las variables globales
+require('dotenv').config();                   // Se utiliza para manejar las variables globales
 
+// Obtencion de las variables de entorno
+const port = process.env.PORT || 4000;
+const usuario_db = process.env.USUARIO_DB;
+const password_db = process.env.PASS_DB;
+const nombre_db = process.env.NOMBRE_DB;
 
-//const port = 3000
-const port = process.env.PORT || 3000;
+// String de conexion a la base de datos
+const conexion_db = `mongodb+srv://${usuario_db}:${password_db}@cluster0.4qowy.mongodb.net/${nombre_db}?retryWrites=true&w=majority`;
 
 const app = express()
-
 
 app.use(express.json());            // Permite obtener el cuerpo del POST via req.body
 
@@ -26,7 +30,7 @@ app.listen(port, () => {
     console.log(`Server corriendo en puerto: ${port}`)
 })
 
-mongoose.connect('mongodb+srv://parcialmcga:mcga123@cluster0.4qowy.mongodb.net/dbparcial?retryWrites=true&w=majority').then((result) => {
+mongoose.connect(conexion_db).then((result) => {
     console.log('Conexion exitosa con la base de datos')
 }).catch((error) => {
     console.log(`Error en la conexion, error:  ${error}`)
